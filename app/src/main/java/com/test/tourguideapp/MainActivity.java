307634632d15e06182e557604c299484a1898d11
PyrovSergey <pyrov.sergey@gmail.com>
2018-03-05 14:15:35 +0300

    update_2

---
+++ app/src/main/java/com/test/tourguideapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.test.tourguideapp;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Получаем ViewPager и устанавливаем в него адаптер
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(
+                new SampleFragmentPagerAdapter(getSupportFragmentManager(), MainActivity.this));
+
+        // Передаём ViewPager в TabLayout
+        TabLayout tabLayout = findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/com/test/tourguideapp/PageFragment.java
@@ -0,0 +1,43 @@
+package com.test.tourguideapp;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class PageFragment extends Fragment {
+
+    public static final String ARG_PAGE = "ARG_PAGE";
+    private int mPage;
+
+    public static PageFragment newInstance(int page) {
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_PAGE, page);
+        PageFragment fragment = new PageFragment();
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mPage = getArguments().getInt(ARG_PAGE);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_page, container, false);
+        TextView textView = (TextView) view;
+        textView.setText("Fragment #" + mPage);
+        return textView;
+    }
+
+}
